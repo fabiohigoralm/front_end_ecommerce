67507e979560e1eb719d1f522c05f26fb5c52862
fabiohigoralm <fabiohigoralm2@gmail.com>
2022-03-06 23:51:43 -0300

    remove img

--- src/components/HeaderProfile.jsx
+++ src/components/HeaderProfile.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import B8oneIcon from "../icon/b8one_icon.svg"
 
 export default function HeaderProfile() {
     return (
-        <div className="Header-profile">
-            <img src={B8oneIcon} alt="B8one"/>
-        </div>
+        <div className="Header-profile"/>
     )
 }
 
